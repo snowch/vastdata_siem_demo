c4dbcb82525cab21237e60718517162d35fbb09e
Chris Snow <chris.snow@vastdata.com>
2025-06-14 09:09:32 +0000

    updated metadata chart

--- bin/export_superset_dashboard.sh
+++ bin/export_superset_dashboard.sh
@@ -6,10 +6,14 @@ set -e
 # Change to the script's directory
 script_dir="$(dirname "$0")"
 
+docker exec -it superset_app rm -f /tmp/siem_dashboard.zip
+
 # Export the Superset dashboard
-docker exec -it superset_app superset export-dashboards -f /tmp/superset_dashboards_export.zip
+docker exec -it superset_app superset export-dashboards -f /tmp/siem_dashboard.zip
+
+sudo rm ${script_dir}/../services/superset/siem_dashboard.zip
 
 # Copy the exported dashboard to the host machine
-docker cp superset_app:/tmp/superset_dashboards_export.zip ${script_dir}/../services/superset/siem_dashboard_export.zip
+docker cp superset_app:/tmp/siem_dashboard.zip ${script_dir}/../services/superset/siem_dashboard.zip
 
-echo "Dashboard exported to: ${script_dir}/../services/superset/superset_dashboards_export.zip"
+echo "Dashboard exported to: ${script_dir}/../services/superset/siem_dashboard.zip"
