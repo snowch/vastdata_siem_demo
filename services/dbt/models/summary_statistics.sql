dfbdb4c909a46b23935b42b6dac2d6ad5eb1cb05
Chris Snow <chris.snow@vastdata.com>
2025-06-18 10:49:57 +0000

    fix meta date

--- services/dbt/models/summary_statistics.sql
+++ services/dbt/models/summary_statistics.sql
@@ -60,3 +60,30 @@ SELECT
     MAX(ts) AS last_updated
 FROM
     {{ source('zeek', 'zeek_dns') }}
+
+UNION ALL
+
+SELECT
+    'fluentd_file_system_activity' AS table_name,
+    COUNT(*) AS total_records,
+    MAX(time_dt) AS last_updated
+FROM
+    {{ source('fluentd', 'fluentd_file_system_activity') }}
+
+UNION ALL
+
+SELECT
+    'fluentd_authentication' AS table_name,
+    COUNT(*) AS total_records,
+    MAX(time_dt) AS last_updated
+FROM
+    {{ source('fluentd', 'fluentd_authentication') }}
+
+UNION ALL
+
+SELECT
+    'fluentd_detection_finding' AS table_name,
+    COUNT(*) AS total_records,
+    MAX(time_dt) AS last_updated
+FROM
+    {{ source('fluentd', 'fluentd_detection_finding') }}
